408b4528150f3c244266183bf3bcfcbe9846cab5
danelekoo7 <danieljedrus@gmail.com>
2020-04-18 13:59:06 +0200

    Task 5 done * added MemoryBookService.java * created methods: getList, getBookById, update, delete * added memoryBook to BookController

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,13 +1,23 @@
 package pl.coderslab.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.model.Book;
+import pl.coderslab.model.MemoryBookService;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    @Autowired
+    private final MemoryBookService memoryBookService;
+
+    public BookController(MemoryBookService memoryBookService) {
+        this.memoryBookService = memoryBookService;
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "{hello: World}";
@@ -18,5 +28,6 @@ public class BookController {
         return new Book(1L, "9788324631766", "Thinking in Java",
                 "Bruce Eckel", "Helion", "programming");
     }
+
 
 }
